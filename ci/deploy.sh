00603dfdb9d503168989afadc4f15019b1c2e6f7
Mario Garcia <mario.ggar@gmail.com>
2018-07-12 00:20:30 +0200

    Add first travis ci draft configuration

---
+++ ci/deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -o pipefail
+set -o errexit
+set -o nounset
+# set -o xtrace
+
+echo $CA_CRT | base64 --decode -i > ${HOME}/ca.crt
+
+kubectl config set-cluster our-k8s-cluster --embed-certs=true --server=${CLUSTER_ENDPOINT} --certificate-authority=${HOME}/ca.crt
+kubectl config set-credentials travis-echo --token=$SA_TOKEN
+kubectl config set-context travis --cluster=$CLUSTER_NAME --user=travis-echo --namespace=rafflethor
+kubectl config use-context travis
+kubectl config current-context
+
+kubectl apply -f ./k8s/deployment.yml
+# kubectl apply -f ./k8s/service.yml
+# kubectl apply -f ./k8s/ingress.yml
+
+function cleanup {
+    printf "Cleaning up...\n"
+    rm -vf "${HOME}/ca.crt"
+    printf "Cleaning done."
+}
+
+trap cleanup EXIT
