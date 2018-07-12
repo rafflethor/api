821a0c9b0fd8594355c5801771a97a0b698cfc5d
Mario Garcia <mario.ggar@gmail.com>
2018-07-12 02:34:18 +0200

    Add snap to install kubectl

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -6,15 +6,15 @@ set -o nounset
 
 echo $CA_CRT | base64 --decode -i > ${HOME}/ca.crt
 
-kubectl config set-cluster our-k8s-cluster --embed-certs=true --server=${CLUSTER_ENDPOINT} --certificate-authority=${HOME}/ca.crt
-kubectl config set-credentials travis-echo --token=$SA_TOKEN
+kubectl config set-cluster ${CLUSTER_NAME} --embed-certs=true --server=${CLUSTER_ENDPOINT} --certificate-authority=${HOME}/ca.crt
+kubectl config set-credentials travis-echo --token=$USER_TOKEN
 kubectl config set-context travis --cluster=$CLUSTER_NAME --user=travis-echo --namespace=rafflethor
 kubectl config use-context travis
 kubectl config current-context
 
-kubectl get nodes
+kubectl get pods --all-namespaces
 
-#kubectl apply -f ./k8s/deployment.yml
+# kubectl apply -f ./k8s/deployment.yml
 # kubectl apply -f ./k8s/service.yml
 # kubectl apply -f ./k8s/ingress.yml
 
