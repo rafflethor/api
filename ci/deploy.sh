4eed0e1bc2f2610b945fa8f55593f31c6507701a
Mario Garcia <mario.ggar@gmail.com>
2018-07-15 20:43:50 +0200

    Fix k8s username variable

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -11,10 +11,11 @@ set -o nounset
 # continuous integration environment.
 #
 # - K8S_CA_CRT: Host certificate
-# - K8S_USER_TOKEN: ServiceAccount token
 # - K8S_CLUSTER_NAME: Cluster name
 # - K8S_HOST: Address name of the K8s cluster
 # - K8S_CONTEXT_NAME: Namespace and context name
+# - K8S_USERNAME: ServiceAccount name
+# - K8S_USER_TOKEN: ServiceAccount token
 #
 # Once the credentials have been set then we can apply the deployment,
 # pods, services...etc yaml files
@@ -26,8 +27,8 @@ K8S_USER_TOKEN=$(echo $K8S_USER_TOKEN | base64 -d)
 
 # SET-CREDENTIALS
 kubectl config set-cluster ${K8S_CLUSTER_NAME} --embed-certs=true --server=${K8S_HOST} --certificate-authority=${HOME}/ca.crt
-kubectl config set-credentials ${K8_USERNAME} --token=${K8S_USER_TOKEN}
-kubectl config set-context ${K8_CONTEXT_NAME} --cluster=${K8S_CLUSTER_NAME} --user=${K8_USERNAME}
+kubectl config set-credentials ${K8S_USERNAME} --token=${K8S_USER_TOKEN}
+kubectl config set-context ${K8_CONTEXT_NAME} --cluster=${K8S_CLUSTER_NAME} --user=${K8S_USERNAME}
 kubectl config use-context ${K8_CONTEXT_NAME}
 
 # DEPLOYMENT
